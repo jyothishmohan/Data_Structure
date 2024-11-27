93ac45d28fa011520b7ded229a75c2d8b6e596d6
jyothishmohan <156563567+jyothishmohan@users.noreply.github.com>
2024-11-27 16:19:20 +0530

    Update double_linked_list.c

--- data_structure/double_linked_list.c
+++ data_structure/double_linked_list.c
@@ -37,16 +37,77 @@ void insert_at_end(int data)
 {
 	struct node *newnode;
 	newnode = create_node(data);
-
-	struct node *ptr=header;
-	while (ptr !=NULL)
+	struct node *ptr;
+	while (ptr->rlink !=NULL)
+	{
+		ptr=ptr->rlink;
+	}
+	if(newnode==NULL)
+	{
+		printf("Not enough memory");
+	}
+	else
 	{
 		ptr->rlink=newnode;
 		newnode->llink=ptr;
 	}
 	
 }
+void insert_at_any(int data, int position)
+    {
+    	struct node *newnode, *ptr;
+    	newnode = create_node(data);
 
+    	if (position == 1)
+    	{
+        	insert_at_front(data);
+        	return;
+    	}	
+
+    	ptr = header;
+    	for (int i = 1; i < position - 1 && ptr != NULL; i++)
+    	{
+        	ptr = ptr->rlink;
+    	}
+
+    	if (ptr == NULL)
+    	{
+        	printf("Position out of bounds\n");
+        	free(newnode);
+    	} 
+     	else
+     	{
+        	newnode->rlink = ptr->rlink;
+        	newnode->llink = ptr;
+        	if (ptr->rlink != NULL)
+        	{
+            		ptr->rlink->llink = newnode;
+        	}
+        	ptr->rlink = newnode;
+    	}
+}
+
+void delete_at_front()
+{
+	struct node *ptr;
+	ptr=header;
+	header=ptr->rlink;
+	free(ptr);
+}
+void delete_at_end()
+{
+     struct node *ptr ;
+     struct node *ptr1;
+     ptr=header;
+     while (ptr->rlink != NULL) 
+     
+     {
+     	ptr1 = ptr;
+        ptr = ptr->rlink;
+     }
+     ptr1->rlink = NULL;
+     free(ptr);
+}
 void traversal()
 {
 	struct node *ptr = header;
@@ -59,12 +120,15 @@ void traversal()
 }
 
 int main()
-{
+{	
+	insert_at_front(22);
 	insert_at_front(22);
 	insert_at_front(33);
 	insert_at_front(44);
+	insert_at_any(333,4);
 	insert_at_end(100);
+	delete_at_front();
+	delete_at_end();
 	traversal();
 	return 0; 
 }
-
